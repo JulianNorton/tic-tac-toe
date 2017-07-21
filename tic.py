b68b6d0c78ca9b63e53c5be45d6568acf938afe3
Julian Norton <Julian@JulianNorton.com>
2017-07-21 16:45:23 -0400

    win conditions checked! :)

--- tic.py
+++ tic.py
@@ -3,50 +3,43 @@ import numpy as np
 
 # create a 3x3 board
 def clean_board():
-
     board = np.zeros((3,3))
     print(board)
 # who goes first?
 # first_player = random.choice(players)
 
 
+# board = np.ones((3,3))
 board = np.zeros((3,3))
-board = np.ones((3,3))
-# board[0,1] = -1
-board[0] = 0
+
+board[0,2] = -1
 board[1,1] = -1
-board[2,2] = -1
+board[2,0] = -1
+
+# board[:,0] = -1
 board_length = len(board)
 
-
-print(board)
 # WINNER CHECK
 for i in range(board_length):
+    print(board[i,i])
     # horizontal winner?
     if abs(np.sum(board[i])) == board_length:
-        # or -board_length
-        print(np.sum(board[i]))
-        print(board_length)
-        print(-board_length)
-        # winner = board[0, i]
-        # print(board[0, i])
-        # print(board, 'b', board_length, -board_length)
-        # print(np.sum(board[i]))
-        # print('i ==',i,'horizontal winner!', winner)
+        print('horizontal winner!')
         break
-    # Horizontal winner?
-    # if np.sum([board[i]], axis=1) == board_length or -board_length:
-    #     winner = board[0, i]
-    #     print(board[0, i])
-    #     print(board)
-    #     print(np.sum(board[i]))
-    #     print('vertical winner!', winner)
-    #     break
-    else:
-        print(board)
-    # if np.sum(board[i]) == 3 or -3:
-    #     winner = board[0, i]
-    #     print('winner!', winner)
-    #     break
+    # vertical winner?
+    if abs(np.sum(board[:,i])) == board_length:
+        print('vertical winner!')
+        break
+    # diagonal winner? \\ top left to bottom right
+    if abs(np.trace(board)) == board_length: 
+        print('diagonal winner!')
+        break
+    # diagonal winner? // top right to bottom left
+
+    if abs(np.trace(np.fliplr(board))) == board_length: 
+        print('diagonal winner!')
+        break
+
+print(board)
 
 
