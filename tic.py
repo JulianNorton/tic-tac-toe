f9a5b3faf04c8d2f29302ed5992241f27aa99271
Julian Norton <julian@juliannorton.com>
2017-07-22 21:54:07 -0400

    return valid moves indices

--- tic.py
+++ tic.py
@@ -19,27 +19,34 @@ board[2,0] = -1
 # board[:,0] = -1
 board_length = len(board)
 
-# WINNER CHECK
-for i in range(board_length):
-    print(board[i,i])
-    # horizontal winner?
-    if abs(np.sum(board[i])) == board_length:
-        print('horizontal winner!')
-        break
-    # vertical winner?
-    if abs(np.sum(board[:,i])) == board_length:
-        print('vertical winner!')
-        break
-    # diagonal winner? \\ top left to bottom right
-    if abs(np.trace(board)) == board_length: 
-        print('diagonal winner!')
-        break
-    # diagonal winner? // top right to bottom left
+def check_winner():
+    # WINNER CHECK
+    for i in range(board_length):
+        print(board[i,i])
+        # horizontal winner?
+        if abs(np.sum(board[i])) == board_length:
+            print('horizontal winner!')
+            break
+        # vertical winner?
+        if abs(np.sum(board[:,i])) == board_length:
+            print('vertical winner!')
+            break
+        # diagonal winner? \\ top left to bottom right
+        if abs(np.trace(board)) == board_length: 
+            print('diagonal winner!')
+            break
+        # diagonal winner? // top right to bottom left
 
-    if abs(np.trace(np.fliplr(board))) == board_length: 
-        print('diagonal winner!')
-        break
+        if abs(np.trace(np.fliplr(board))) == board_length: 
+            print('diagonal winner!')
+            break
+def valid_moves():
+    return np.transpose(np.where(board==0))
 
+check_winner()
+
+
+print(valid_moves())
 print(board)
 
 
