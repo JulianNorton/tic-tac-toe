ffd8a86af38156d5152e66660f13eed05982fe9d
Julian Norton <julian@juliannorton.com>
2017-07-23 20:20:36 -0400

    two players playing!

--- tic.py
+++ tic.py
@@ -6,28 +6,25 @@ random.seed(1)
 
 # create a 3x3 board
 def clean_board():
-    board = np.zeros((3,3))
+    board = np.zeros((5,5))
     print(board)
 # who goes first?
 # first_player = random.choice(players)
+
 player_one = 1
+player_two = -1
 
-
-# board = np.ones((3,3))
-board = np.zeros((3,3))
-
-# board[0,2] = -1
-# board[1,1] = -1
-# board[2,0] = -1
-
-# board[:,0] = -1
+board = np.zeros((5,5))
 board_length = len(board)
+
+
+
 
 def check_winner():
     # WINNER CHECK
     game_over = False
     for i in range(board_length):
-        print(board[i,i])
+
         # horizontal winner?
         if abs(np.sum(board[i])) == board_length:
             print('horizontal winner!')
@@ -63,26 +60,24 @@ def moves_selection(player):
         print(select_move)
         select_move = list(select_move)
         board[select_move[0],select_move[1]] = player
-        print('making a move!')
+        print('(',player,')', 'making a move!')
     else:
-        print('no valid moves, tie.')
-        print(board)
+        print('no valid moves')
 
 
-print(moves_validate())
-moves_selection(player_one)
-print(board)
 
-
-game_over = check_winner()
-
-while game_over == False:
-    moves_selection(player_one)
-    print('game not over!!!')
-    print(board)
+def game_iteration():
     game_over = check_winner()
-    if game_over == True:
-        break
+    while game_over == False:
+        print(board)
+        moves_selection(player_one)
+        print(board)
+        moves_selection(player_two)
+        print(board, '\n')
+        game_over = check_winner()
+        if game_over == True:
+            print('\n', 'game over')
+            print(board)
+            break
 
-
-
+game_iteration()
