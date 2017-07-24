3b94fac04139e9489b0e9bab333648926e316919
Julian Norton <julian@juliannorton.com>
2017-07-23 21:12:03 -0400

    cleaning up code

--- tic.py
+++ tic.py
@@ -3,22 +3,15 @@ import numpy as np
 
 random.seed(1)
 
-
-# create a 3x3 board
+# create a square board
 def clean_board():
-    board = np.zeros((5,5))
-    print(board)
-# who goes first?
-# first_player = random.choice(players)
+    return np.zeros((3,3))
 
 player_one = 1
 player_two = -1
 
-board = np.zeros((5,5))
+board = clean_board()
 board_length = len(board)
-
-
-
 
 def check_winner():
     # WINNER CHECK
@@ -29,27 +22,32 @@ def check_winner():
         if abs(np.sum(board[i])) == board_length:
             print('horizontal winner!')
             game_over = True
-            break
+            print('game over')
+            quit()
 
         # vertical winner?
         if abs(np.sum(board[:,i])) == board_length:
             print('vertical winner!')
             game_over = True
-            break
+            print('game over')
+            quit()
         # diagonal winner? \\ top left to bottom right
 
         if abs(np.trace(board)) == board_length: 
             print('diagonal winner!')
             game_over = True
-            break
+            print('game over')
+            quit()
 
         # diagonal winner? // top right to bottom left
         if abs(np.trace(np.fliplr(board))) == board_length: 
             print('diagonal winner!')
             game_over = True
-            break
+            print('game over')
+            quit()
     return game_over
 
+# Finds the '0's on the board and returns their location via indices
 def moves_validate():
     return np.transpose(np.where(board==0))
 
@@ -63,11 +61,10 @@ def moves_selection(player):
         print('(',player,')', 'making a move!')
     else:
         print('no valid moves')
-
-
+        
 
 def game_iteration():
-    game_over = check_winner()
+    game_over = False
     while game_over == False:
         print(board)
         moves_selection(player_one)
@@ -75,9 +72,5 @@ def game_iteration():
         moves_selection(player_two)
         print(board, '\n')
         game_over = check_winner()
-        if game_over == True:
-            print('\n', 'game over')
-            print(board)
-            break
 
 game_iteration()
