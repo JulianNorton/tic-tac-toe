ddc35b7a9cd7eea2c095b84757cebe645ffd9b3f
Julian Norton <Julian@JulianNorton.com>
2017-08-05 13:42:20 -0400

    added human and modifying main class

---
+++ human.py
@@ -0,0 +1,22 @@
+class Human:
+    def __init__(self):
+    pass
+
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+
+    def take_action(self, env):
+        while True:
+            move = input('enter coordinates as "i,j"')
+            i, j = move.split(',')
+            i = int(i)
+            j = int(j)
+            if env.is_empty(i,j):
+                env.board[i,j] = self.symbol
+                break
+
+    def update(self, env):
+        pass
+        
+    def update_state_history(self, s):
+        pass
--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import reward
 
 print('test2')
 
-b = agent.agent(2,2)
+# agent_1 = agent.agent(.01,.01)
 
-c = agent.setV(b,5)
+# c = agent.setV(b,5)
+
+def play_game(player_one, player_two, env, display=False):
+    # loops until game is over
+    current_player = None
+    while not env.game_over():
+        # alternate between players
+        # p1 always first
+        if current_player == player_one:
+            current_player == player_two
+        else:
+            current_player == player_one
+
+        if display == true:
+            print('test')
